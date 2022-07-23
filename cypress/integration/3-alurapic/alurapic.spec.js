adb6d0d53739ad46e0225e7ba07fac7c7a3d79d2
Wagner <wagner_silvajr@hotmail.com>
2022-07-22 22:10:22 -0300

    checking DOM elements of register validation

--- cypress/integration/3-alurapic/alurapic.spec.js
+++ cypress/integration/3-alurapic/alurapic.spec.js
@@ -13,4 +13,37 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
     })
+
+    it('verifica email invalido', () =>{
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('wagner')
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');        
+    })
+
+    it('verifica nome invalido', () =>{
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="fullName"]').type('a')
+        cy.contains('button', 'Register').click();      
+        cy.contains('ap-vmessage', 'Mininum lenght is 2').should('be.visible');        
+    })
+
+    it('verifica nome de usuario invalido', () =>{
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('b')   
+        cy.contains('button', 'Register').click();     
+        cy.contains('ap-vmessage', 'Mininum lenght is 2').should('be.visible');        
+    })
+
+    it('verifica senha invalida', () =>{
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123')
+        cy.contains('button', 'Register').click();        
+        cy.contains('ap-vmessage', 'Mininum lenght is 8').should('be.visible');        
+    })
+
+    
 })
